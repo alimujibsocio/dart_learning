fa5b4b89d496b6b9305623c5da5846a8077327a9
Ali Socio <alimujib678@gmail.com>
2022-06-30 17:31:33 +0700

    while

---
+++ bin/22.while.dart
@@ -0,0 +1,11 @@
+void main() {
+  /*While loop hanya ada kondisi perulangan tidak ada init statement dan post statement,
+  lebih simple dari for loop
+  */
+
+  var counter = 1;
+  while (counter <= 10) {
+    print('perulangan ke-$counter');
+    counter++;
+  }
+}
